1090800af74cb3b2c86422c01c1dbbe36f65d778
imiftakhov <ilias.miftakhov@yandex.ru>
2023-04-26 10:33:56 +0200

    добавлены презентации в pdf для курса по алгоритмам

--- src/basiс/HW.java
+++ src/basiс/HW.java
@@ -1,74 +1,44 @@
 package basiс;
 
-// methods and encapsulation
-// LocalDate
-// inheritance
-
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class HW {
-    private static final Scanner SCANNER = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        String string = "MXLIX"; // 1049
-        System.out.println(romanToDecimal(string));
-    } //
+        System.out.println("Enter count of boxes please:");
+        int countOfBoxes = 3;
+        System.out.println("Enter weight of each single box in kg:");
+        double weightOfSingleBox = 1.3;
+        System.out.println("Enter please full name of recipient:");
+        //scanner.nextLine(); // stub
+        String nameOfRecipient = "John Smith";
+        System.out.println("Enter please address of recipient:");
+        String addressOfRecipient = "White str 31";
 
-    private static int romanToDecimal(String romanNumeral) {
-//        HashMap<Character, Integer> map = new HashMap<>();
-//        map.put('I', 1);
-//        map.put('V', 5);
-//        map.put('X', 10);
-//        map.put('L', 50);
-//        map.put('C', 100);
-//        map.put('D', 500);
-//        map.put('M', 100);
+        double totalParcelWeight = countOfBoxes * weightOfSingleBox;
 
+        String label =
+                createLabel(nameOfRecipient, addressOfRecipient, totalParcelWeight);
 
-        // "VII"
-
-        int end = romanNumeral.length()- 1; // 2
-        char[] arr = romanNumeral.toCharArray(); // {'V', 'I', 'I'}
-        int current;
-        int result = mapping(romanNumeral.charAt(end)); // charAt(2) = 'I' -> 1
-
-
-        for (int i = end - 1; i >= 0; i--) { //цикл идет справа на лево
-            current = mapping(romanNumeral.charAt(i)); // charAt(0) = 'V' -> 5
-            int prev = mapping(romanNumeral.charAt(i + 1));
-
-
-            if (current >= prev) { //сравниваем больше или меньше число числа которое слева, если меньше то вычитаем
-                result = result + current; // 7
-            } else {
-                result -= current;
-            }
-
-        }
-        return result; // 7
+        System.out.println(label);
     }
 
 
-    static int mapping(char c) {
-        switch (c) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'C':
-                return 100;
-            case 'L':
-                return 50;
-            case 'M':
-                return 1000;
-            default:
-                return 0;
-        }
+    static String createLabel(String name, String address, double weight) {
+        String ticket = "\nRecipient name: \"" + name.toUpperCase() + "\"\n"
+                + "Recipient address: \"" + address.toUpperCase() + "\"\n"
+                + "Parcel weight: " + weight + " kg";
+
+        return ticket;
     }
+
+
+
+
+
 }
 
